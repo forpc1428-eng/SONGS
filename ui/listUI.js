56c282ac0ae2800c1e322e3df29dc8a9ce7155e9
forpc1428-eng <forpc1428@gmail.com>
2026-03-18 12:35:19 +0530

    Delete ui directory

--- ui/listUI.js
+++
@@ -1,9 +0,0 @@
-export function renderList(songs){
-    const list = document.getElementById("list");
-
-    list.innerHTML = songs.map((s,i)=>`
-        <div class="track" onclick="playSong(${i})">
-            ${i+1}. ${s.name}
-        </div>
-    `).join("");
-}
