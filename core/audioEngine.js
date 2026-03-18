79e61f5689b356ec640c6dabeddeee32ce96e1ea
forpc1428-eng <forpc1428@gmail.com>
2026-03-18 12:31:21 +0530

    Update audioEngine.js

--- core/audioEngine.js
+++ core/audioEngine.js
@@ -1,24 +1,22 @@
+let analyzer, dataArray;
+
 export function initAudioEngine(){
     const audio = document.getElementById("audio");
 
     const ctx = new (window.AudioContext || window.webkitAudioContext)();
     const src = ctx.createMediaElementSource(audio);
 
-    const analyzer = ctx.createAnalyser();
+    analyzer = ctx.createAnalyser();
     src.connect(analyzer);
     analyzer.connect(ctx.destination);
 
     analyzer.fftSize = 64;
+    dataArray = new Uint8Array(analyzer.frequencyBinCount);
+}
 
-    const dataArray = new Uint8Array(analyzer.frequencyBinCount);
+export function getBass(){
+    if(!analyzer) return 0;
 
-    function tick(){
-        requestAnimationFrame(tick);
-        analyzer.getByteFrequencyData(dataArray);
-
-        const bass = dataArray[2] / 255;
-        console.log("Bass:", bass);
-    }
-
-    tick();
+    analyzer.getByteFrequencyData(dataArray);
+    return dataArray[2]/255;
 }
